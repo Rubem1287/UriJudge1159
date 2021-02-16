852ff555f535b6ba6d53f0374c70dc0cc98efb01
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:40:35 -0300

    Add files via upload

---
+++ Uri1159/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Uri1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,total,cont,n;
+
+            cont = 0;
+            
+            a = int.Parse(Console.ReadLine());
+            total = a;
+            if (a%2==0&&a!=0)
+            {
+                while (cont != 4)
+                {
+                    a += +2;
+                    total += +a;
+                    cont += +1;
+
+                }
+
+            }
+            else if(a!=0)
+            {
+                a += +1;
+                total += +1;
+                while (cont != 4)
+                {
+                    a += +2;
+                    total += +a;
+                    cont += +1;
+                }
+
+            }
+
+
+            Console.WriteLine(total);
+                    
+
+        }
+    }
+}
